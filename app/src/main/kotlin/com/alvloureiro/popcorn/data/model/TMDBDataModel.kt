d776e616b6fb1f4a6a30234e08baa5ee6a8c9656
Andre Loureiro <loureiro.andrew@gmail.com>
2018-03-06 17:52:52 -0400

    [Model]: Refactor by pass the API's return type
    Refactor the model return type to fit with the API's return type, so
    this way we don't need to filter or map anything.
    
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

--- app/src/main/kotlin/com/alvloureiro/popcorn/data/model/TMDBDataModel.kt
+++ app/src/main/kotlin/com/alvloureiro/popcorn/data/model/TMDBDataModel.kt
@@ -1,29 +1,21 @@
 package com.alvloureiro.popcorn.data.model
 
 import com.alvloureiro.popcorn.api.MoviesDataBase
-import com.alvloureiro.popcorn.data.valueobjects.Genre
-import com.alvloureiro.popcorn.data.valueobjects.Movie
+import com.alvloureiro.popcorn.data.valueobjects.GenresResult
+import com.alvloureiro.popcorn.data.valueobjects.Result
 import io.reactivex.Observable
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class TMDBDataModel @Inject constructor(private val moviesDataBase: MoviesDataBase){
-    fun upComingMovies(page: Int = 1): Observable<List<Movie>>? {
-        return moviesDataBase.upcomingMovies(page)
-                ?.filter { result -> (result.results?.isNotEmpty() as Boolean)}
-                ?.map { result -> (result.results) }
-    }
+    fun upComingMovies(page: Int = 1): Observable<Result>? = moviesDataBase.upcomingMovies(page)
 
-    fun genres(): Observable<List<Genre>>? {
+    fun genres(): Observable<GenresResult>? {
         return moviesDataBase.genres()
-                ?.filter { result ->  (result.genres?.isNotEmpty() as Boolean)}
-                ?.map { result -> (result.genres) }
     }
 
-    fun searchMovie(movieTitle: String, page: Int): Observable<List<Movie>>? {
-        return moviesDataBase.searchMovie(movieTitle, page)
-                ?.filter { result -> (result.results?.isNotEmpty() as Boolean)}
-                ?.map { result -> result.results }
-    }
+
+    fun searchMovie(movieTitle: String, page: Int): Observable<Result>? =
+        moviesDataBase.searchMovie(movieTitle, page)
 }
